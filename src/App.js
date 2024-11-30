1d2d22bc9d18a667dc01cd36abce57b00b106b51
alokkumar-dev <alokshakya2601@gmail.com>
2024-11-30 11:13:50 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import 'react-toastify/dist/ReactToastify.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { CreateQuotePage, LoginPage, Navbar, ProtractedRoute, QuoteListPage } from './components';
 import { ToastContainer } from 'react-toastify';
 
 function App() {
+  const isLoggedIn = localStorage.getItem('authToken');
+
   return (
     <Router>
       <div className="min-h-screen">
@@ -27,6 +29,12 @@ function App() {
               <ProtractedRoute>
                 <CreateQuotePage />
               </ProtractedRoute>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              isLoggedIn ? <Navigate to="/quotes" /> : <Navigate to="/" />
             }
           />
         </Routes>
